cc72083f436ed1c25ddd5329241d8fe629785bfe
sipu-ss <chinmaya257kumar@gmail.com>
2019-12-29 23:26:51 +0530

    added new project

---
+++ LittleItishree/src/main/java/com/sipu/little/LittleItishree/LittleItishreeApplication.java
@@ -0,0 +1,13 @@
+package com.sipu.little.LittleItishree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleItishreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleItishreeApplication.class, args);
+	}
+
+}
---
+++ LittleItishree/src/test/java/com/sipu/little/LittleItishree/LittleItishreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipu.little.LittleItishree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleItishreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
